b7e7a016bcd1f49879b797cb54140f0fdbdfe8be
engeir <engeir@pm.me>
2024-02-12 16:10:57 +0100

    fix(manipulate): add func to module init

--- src/volcano_base/manipulate/__init__.py
+++ src/volcano_base/manipulate/__init__.py
@@ -9,19 +9,21 @@ from volcano_base.manipulate.time_series import (
     normalize_peaks,
     remove_seasonality,
     shift_arrays,
+    subtract_mean_of_tail,
     weighted_season_avg,
     weighted_year_avg,
 )
 
 __all__ = [
-    "shift_arrays",
-    "mean_flatten",
-    "remove_seasonality",
     "dt2float",
     "float2dt",
     "get_median",
     "keep_whole_years",
-    "weighted_year_avg",
-    "weighted_season_avg",
+    "mean_flatten",
     "normalize_peaks",
+    "remove_seasonality",
+    "shift_arrays",
+    "subtract_mean_of_tail",
+    "weighted_season_avg",
+    "weighted_year_avg",
 ]
