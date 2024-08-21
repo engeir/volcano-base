1626705ba1e868031a3dd821c0cf3ed01a34ef50
engeir <engeir@pm.me>
2024-08-21 14:28:15 +0200

    fix(load): make mcg24 functions available

--- src/volcano_base/load.pyi
+++ src/volcano_base/load.pyi
@@ -1,4 +1,5 @@
 from volcano_base.load.load_c2w_files import FindFiles as FindFiles, RegexLookup as RegexLookup, default_regex as default_regex, nird_archive_regex as nird_archive_regex
+from volcano_base.load.load_mcg24_files import find_mcg24_files as find_mcg24_files
 from volcano_base.load.load_ob16 import OttoBliesner as OttoBliesner
 
-__all__ = ['FindFiles', 'OttoBliesner', 'RegexLookup', 'default_regex', 'nird_archive_regex']
+__all__ = ['FindFiles', 'OttoBliesner', 'RegexLookup', 'default_regex', 'find_mcg24_files', 'nird_archive_regex']
--- src/volcano_base/load/__init__.py
+++ src/volcano_base/load/__init__.py
@@ -6,6 +6,7 @@ from volcano_base.load.load_c2w_files import (
     default_regex,
     nird_archive_regex,
 )
+from volcano_base.load.load_mcg24_files import find_mcg24_files
 from volcano_base.load.load_ob16 import OttoBliesner
 
 __all__ = [
@@ -13,5 +14,6 @@ __all__ = [
     "OttoBliesner",
     "RegexLookup",
     "default_regex",
+    "find_mcg24_files",
     "nird_archive_regex",
 ]
